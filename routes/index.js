1cc5e55d8da1f256447ad6605091ff012a372472
Michaela Wagner <mickywagner@gmail.com>
2020-05-16 18:32:36 -0600

    Add bootstrap, create basic sign-up form

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup')
+})
 
 module.exports = router;
