2d043174bdf4ac9a1cc5614c39e1dae8384e4185
Michaela Wagner <mickywagner@gmail.com>
2020-06-02 16:02:27 -0600

    Prevent authenticated users from accessing login/sign up routes

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,27 @@ initialize(passport)
 const userController = require('../controllers/userController')
 const messageController = require('../controllers/messagesController')
 
+function checkNotAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    res.redirect('/')
+  }
+  next()
+}
+
 router.use(function(req, res, next) {
   res.locals.currentUser = req.user
   next()
 })
-
 /* GET home page. */
 router.get('/', messageController.message_list)
 
-router.get('/signup', (req, res, next) => {
+router.get('/signup', checkNotAuthenticated, (req, res, next) => {
   res.render('signup')
 })
 
 router.post('/signup', userController.create_user_post)
 
-router.get('/login', (req, res, next) => {
+router.get('/login', checkNotAuthenticated, (req, res, next) => {
   res.render('login')
 })
 
